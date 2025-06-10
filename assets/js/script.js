e42f46b83bc97f1fd630760c4647da2a97dd594d
Vijay M N <125730179+CJVijay1999@users.noreply.github.com>
2025-06-10 18:59:52 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+let date = document.getElementById("date");
+      let time = document.getElementById("time");
+   
+      function setTime() {
+        let now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        let seconds = now.getSeconds();
+        let month = now.getMonth() + 1;
+        let day = now.getDate();
+        let year = now.getFullYear();
+        let ampm = hours >= 12 ? "PM" : "AM";
+        if (hours < 10) {
+          hours = "0" + hours;
+        }
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+        if (month < 10) {
+          month = "0" + month;
+        }
+        if (day < 10) {
+          day = "0" + day;
+        }
+
+        let currentTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
+        let currentDay = day + "/" + month + "/" + year;
+        date.innerHTML = currentDay;
+        time.innerHTML = currentTime;
+      }
+
+
+
+      setInterval(setTime, );
+      setTime()
